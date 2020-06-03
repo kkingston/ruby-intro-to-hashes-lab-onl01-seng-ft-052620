d73eaf7a08b3ba192c71f444f948425dd21f87c5
Kayla Kingston <kkingston09@gmail.com>
2020-06-03 22:35:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
 	  monopoly = {
 	  railroads: {pieces: 4, 
 	              names: {reading_railroad: {},
-	                      pennsylvania_railroad: {}
+	                      pennsylvania_railroad: {},
 	                       b_and_o_railroad: {}
 	              }, 
 	                      
