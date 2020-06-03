c9515e512d0fd0745518e6236a00a335b2a4897a
Kayla Kingston <kkingston09@gmail.com>
2020-06-03 22:23:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,9 +21,10 @@ end
 
 def monopoly_with_third_tier
 	 monopoly = {
-	  railroads: {pieces: 4, names: {}},
-	  names: {},
-	  
+	  railroads: {pieces: 4, 
+	              names: {}
+	    
+	  }
 	}
 end
 
